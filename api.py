778b0ac1edce160651c6abd61c128c725ef725d9
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-11-06 12:16:32 +0100

    add deep learning and api

--- api.py
+++ api.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, jsonify
 from ml import moderate_comment
+from ia import get_toxicity
 
 app = Flask(__name__)
+
+# @app.route('/predict_sentiment', methods=['POST'])
+# def predict_sentiment():
+#     data = request.json
+#     comment = data.get('comment', '') if data else ''
+#     result = moderate_comment(comment)
+#     return jsonify(result)
 
 @app.route('/predict_sentiment', methods=['POST'])
 def predict_sentiment():
     data = request.json
     comment = data.get('comment', '') if data else ''
-    result = moderate_comment(comment)
+    result = get_toxicity(comment)
     return jsonify(result)
 
 if __name__ == '__main__':
---
+++ ia.py
@@ -0,0 +1,70 @@
+import os
+import pandas as pd
+import numpy as np
+import string
+import spacy
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import class_weight
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense
+from keras.models import load_model
+from deep_translator import GoogleTranslator
+
+nlp = spacy.load("en_core_web_sm")
+tokenizer = Tokenizer()
+variables = ["toxic", "obscene", "insult", "identity_hate"]
+
+def preprocess_text(text):
+    text = re.sub(r"<.*?>", "", text)
+    doc = nlp(text, disable=["parser", "tagger", "ner", "textcat"])
+    simplified_text = " ".join([token.lemma_ for token in doc if not token.is_punct and not token.is_stop and not token.is_digit])
+    
+    return simplified_text
+
+def clear_data(data, var):
+    data_copy = data.copy()
+
+    data_copy["comment_text"] = data_copy["comment_text"].astype(str)
+    data_copy = data_copy.drop_duplicates(subset="comment_text")
+    data_copy["comment_text"] = data_copy["comment_text"].dropna()
+
+    toxic_samples = data_copy[data_copy[var] == 1].drop_duplicates(subset="comment_text")
+    non_toxic_samples = data_copy[data_copy[var] == 0].drop_duplicates(subset="comment_text")
+    num_toxic_samples = min(50000, len(toxic_samples))
+    toxic_samples = toxic_samples.sample(n=num_toxic_samples, random_state=42)
+    non_toxic_samples = non_toxic_samples.sample(n=num_toxic_samples, random_state=42)
+    new_data = pd.concat([toxic_samples, non_toxic_samples])
+
+    data = new_data.sample(frac=1, random_state=42)
+    data["comment_text"] = data["comment_text"].apply(preprocess_text)
+
+    return data
+
+def prediction_toxicity(model, comment):
+    max_sequence_length = 100
+    comment_sequence = tokenizer.texts_to_sequences([comment])
+    comment_padded = pad_sequences(comment_sequence, maxlen=max_sequence_length, padding='post')
+
+    predictions = model.predict(comment_padded)
+    class_labels = [1, 0]
+    predicted_class = class_labels[np.argmax(predictions)]
+
+    return predicted_class, predictions
+
+def get_toxicity(comment):
+    toxic_score = 0
+
+    for toxic_var in variables:
+        model = load_model(f"static/model/{toxic_var}.keras")
+        comment = GoogleTranslator(source='auto', target='en').translate(comment)
+        comment = preprocess_text(comment)
+
+        predicted_class, predictions = prediction_toxicity(model, comment)
+        print(f"{toxic_var} : {predicted_class} : {predictions}")
+        toxic_score = toxic_score + predicted_class
+    
+    return toxic_score
